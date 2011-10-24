1b6067e567904554165ec0f286549bcb48c9b126
Michael Spencer <bigcheesegs@gmail.com>
2011-10-24 09:55:20 -0700

    stuff

--- tools/lold/lold.cpp
+++ tools/lold/lold.cpp
@@ -14,7 +14,22 @@
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/PrettyStackTrace.h"
 #include "llvm/Support/Signals.h"
+#include <vector>
 using namespace llvm;
+
+class Link;
+
+class Atom {
+public:
+  virtual ~Atom() {}
+
+  std::vector<Link> Links;
+};
+
+class Link {
+public:
+  virtual ~Link() {}
+};
 
 int main(int argc, char **argv) {
   // Print a stack trace if we signal out.
