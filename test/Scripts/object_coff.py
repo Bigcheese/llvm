c735d40f2f1bff4adeb05c90a5e3e724e59b492b
Michael Spencer <bigcheesegs@gmail.com>
2011-12-12 12:06:47 -0800

    dfsa

--- test/Scripts/object_coff.py
+++ test/Scripts/object_coff.py
@@ -69,7 +69,7 @@ file = [('struct', 'header', '!Header', [
     ('PointerToLineNumbers', ('scalar',  '<L',  '0x%X' )),
     ('NumberOfRelocations',  ('scalar',  '<H',  '%d'   )),
     ('NumberOfLineNumbers',  ('scalar',  '<H',  '%d'   )),
-    ('Charateristics',       ('flags',   '<L',  '0x%X', [
+    ('Characteristics',      ('flags',   '<L',  '0x%X', [
       (0x00000008, 'IMAGE_SCN_TYPE_NO_PAD'),
       (0x00000020, 'IMAGE_SCN_CNT_CODE'),
       (0x00000040, 'IMAGE_SCN_CNT_INITIALIZED_DATA'),
@@ -592,35 +592,187 @@ SIZEOF_RELOCATION = 10
 SIZEOF_SYMBOL     = 18
 
 MachineTypes = {
-    'IMAGE_FILE_MACHINE_I386': 0x14C
+    'IMAGE_FILE_MACHINE_UNKNOWN':   0x0,
+    'IMAGE_FILE_MACHINE_AM33':      0x13,
+    'IMAGE_FILE_MACHINE_AMD64':     0x8664,
+    'IMAGE_FILE_MACHINE_ARM':       0x1C0,
+    'IMAGE_FILE_MACHINE_ARMV7':     0x1C4,
+    'IMAGE_FILE_MACHINE_EBC':       0xEBC,
+    'IMAGE_FILE_MACHINE_I386':      0x14C,
+    'IMAGE_FILE_MACHINE_IA64':      0x200,
+    'IMAGE_FILE_MACHINE_M32R':      0x9041,
+    'IMAGE_FILE_MACHINE_MIPS16':    0x266,
+    'IMAGE_FILE_MACHINE_MIPSFPU':   0x366,
+    'IMAGE_FILE_MACHINE_MIPSFPU16': 0x466,
+    'IMAGE_FILE_MACHINE_POWERPC':   0x1F0,
+    'IMAGE_FILE_MACHINE_POWERPCFP': 0x1F1,
+    'IMAGE_FILE_MACHINE_R4000':     0x166,
+    'IMAGE_FILE_MACHINE_SH3':       0x1A2,
+    'IMAGE_FILE_MACHINE_SH3DSP':    0x1A3,
+    'IMAGE_FILE_MACHINE_SH4':       0x1A6,
+    'IMAGE_FILE_MACHINE_SH5':       0x1A8,
+    'IMAGE_FILE_MACHINE_THUMB':     0x1C2,
+    'IMAGE_FILE_MACHINE_WCEMIPSV2': 0x169
   }
 
 HeaderChars = {
+    'IMAGE_FILE_RELOCS_STRIPPED':         0x0001,
+    'IMAGE_FILE_EXECUTABLE_IMAGE':        0x0002,
+    'IMAGE_FILE_LINE_NUMS_STRIPPED':      0x0004,
+    'IMAGE_FILE_LOCAL_SYMS_STRIPPED':     0x0008,
+    'IMAGE_FILE_AGGRESSIVE_WS_TRIM':      0x0010,
+    'IMAGE_FILE_LARGE_ADDRESS_AWARE':     0x0020,
+    'IMAGE_FILE_BYTES_REVERSED_LO':       0x0080,
+    'IMAGE_FILE_32BIT_MACHINE':           0x0100,
+    'IMAGE_FILE_DEBUG_STRIPPED':          0x0200,
+    'IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP': 0x0400,
+    'IMAGE_FILE_NET_RUN_FROM_SWAP':       0x0800,
+    'IMAGE_FILE_SYSTEM':                  0x1000,
+    'IMAGE_FILE_DLL':                     0x2000,
+    'IMAGE_FILE_UP_SYSTEM_ONLY':          0x4000,
+    'IMAGE_FILE_BYTES_REVERSED_HI':       0x8000
   }
 
 SecChars = {
-    'IMAGE_SCN_CNT_CODE':      0x00000020,
-    'IMAGE_SCN_ALIGN_16BYTES': 0x00500000,
-    'IMAGE_SCN_MEM_EXECUTE':   0x20000000,
-    'IMAGE_SCN_MEM_READ':      0x40000000
+    'IMAGE_SCN_TYPE_NO_PAD':            0x00000008,
+    'IMAGE_SCN_CNT_CODE':               0x00000020,
+    'IMAGE_SCN_CNT_INITIALIZED_DATA':   0x00000040,
+    'IMAGE_SCN_CNT_UNINITIALIZED_DATA': 0x00000080,
+    'IMAGE_SCN_LNK_OTHER':              0x00000100,
+    'IMAGE_SCN_LNK_INFO':               0x00000200,
+    'IMAGE_SCN_LNK_REMOVE':             0x00000800,
+    'IMAGE_SCN_LNK_COMDAT':             0x00001000,
+    'IMAGE_SCN_GPREL':                  0x00008000,
+    'IMAGE_SCN_MEM_PURGEABLE':          0x00020000,
+    'IMAGE_SCN_MEM_16BIT':              0x00020000,
+    'IMAGE_SCN_MEM_LOCKED':             0x00040000,
+    'IMAGE_SCN_MEM_PRELOAD':            0x00080000,
+    'IMAGE_SCN_ALIGN_1BYTES':           0x00100000,
+    'IMAGE_SCN_ALIGN_2BYTES':           0x00200000,
+    'IMAGE_SCN_ALIGN_4BYTES':           0x00300000,
+    'IMAGE_SCN_ALIGN_8BYTES':           0x00400000,
+    'IMAGE_SCN_ALIGN_16BYTES':          0x00500000,
+    'IMAGE_SCN_ALIGN_32BYTES':          0x00600000,
+    'IMAGE_SCN_ALIGN_64BYTES':          0x00700000,
+    'IMAGE_SCN_ALIGN_128BYTES':         0x00800000,
+    'IMAGE_SCN_ALIGN_256BYTES':         0x00900000,
+    'IMAGE_SCN_ALIGN_512BYTES':         0x00A00000,
+    'IMAGE_SCN_ALIGN_1024BYTES':        0x00B00000,
+    'IMAGE_SCN_ALIGN_2048BYTES':        0x00C00000,
+    'IMAGE_SCN_ALIGN_4096BYTES':        0x00D00000,
+    'IMAGE_SCN_ALIGN_8192BYTES':        0x00E00000,
+    'IMAGE_SCN_LNK_NRELOC_OVFL':        0x01000000,
+    'IMAGE_SCN_MEM_DISCARDABLE':        0x02000000,
+    'IMAGE_SCN_MEM_NOT_CACHED':         0x04000000,
+    'IMAGE_SCN_MEM_NOT_PAGED':          0x08000000,
+    'IMAGE_SCN_MEM_SHARED':             0x10000000,
+    'IMAGE_SCN_MEM_EXECUTE':            0x20000000,
+    'IMAGE_SCN_MEM_READ':               0x40000000,
+    'IMAGE_SCN_MEM_WRITE':              0x80000000
   }
 
 SimpleType = {
-    'IMAGE_SYM_TYPE_NULL': 0
+    'IMAGE_SYM_TYPE_NULL':   0,
+    'IMAGE_SYM_TYPE_VOID':   1,
+    'IMAGE_SYM_TYPE_CHAR':   2,
+    'IMAGE_SYM_TYPE_SHORT':  3,
+    'IMAGE_SYM_TYPE_INT':    4,
+    'IMAGE_SYM_TYPE_LONG':   5,
+    'IMAGE_SYM_TYPE_FLOAT':  6,
+    'IMAGE_SYM_TYPE_DOUBLE': 7,
+    'IMAGE_SYM_TYPE_STRUCT': 8,
+    'IMAGE_SYM_TYPE_UNION':  9,
+    'IMAGE_SYM_TYPE_ENUM':   10,
+    'IMAGE_SYM_TYPE_MOE':    11,
+    'IMAGE_SYM_TYPE_BYTE':   12,
+    'IMAGE_SYM_TYPE_WORD':   13,
+    'IMAGE_SYM_TYPE_UINT':   14,
+    'IMAGE_SYM_TYPE_DWORD':  15
   }
 
 ComplexType = {
     'IMAGE_SYM_DTYPE_NULL':     0,
-    'IMAGE_SYM_DTYPE_FUNCTION': 2
+    'IMAGE_SYM_DTYPE_POINTER':  1,
+    'IMAGE_SYM_DTYPE_FUNCTION': 2,
+    'IMAGE_SYM_DTYPE_ARRAY':    3,
   }
 
 StorageClass = {
-    'IMAGE_SYM_CLASS_EXTERNAL': 2,
-    'IMAGE_SYM_CLASS_STATIC':   3
+    'IMAGE_SYM_CLASS_END_OF_FUNCTION':  -1,
+    'IMAGE_SYM_CLASS_NULL':             0,
+    'IMAGE_SYM_CLASS_AUTOMATIC':        1,
+    'IMAGE_SYM_CLASS_EXTERNAL':         2,
+    'IMAGE_SYM_CLASS_STATIC':           3,
+    'IMAGE_SYM_CLASS_REGISTER':         4,
+    'IMAGE_SYM_CLASS_EXTERNAL_DEF':     5,
+    'IMAGE_SYM_CLASS_LABEL':            6,
+    'IMAGE_SYM_CLASS_UNDEFINED_LABEL':  7,
+    'IMAGE_SYM_CLASS_MEMBER_OF_STRUCT': 8,
+    'IMAGE_SYM_CLASS_ARGUMENT':         9,
+    'IMAGE_SYM_CLASS_STRUCT_TAG':       10,
+    'IMAGE_SYM_CLASS_MEMBER_OF_UNION':  11,
+    'IMAGE_SYM_CLASS_UNION_TAG':        12,
+    'IMAGE_SYM_CLASS_TYPE_DEFINITION':  13,
+    'IMAGE_SYM_CLASS_UNDEFINED_STATIC': 14,
+    'IMAGE_SYM_CLASS_ENUM_TAG':         15,
+    'IMAGE_SYM_CLASS_MEMBER_OF_ENUM':   16,
+    'IMAGE_SYM_CLASS_REGISTER_PARAM':   17,
+    'IMAGE_SYM_CLASS_BIT_FIELD':        18,
+    'IMAGE_SYM_CLASS_BLOCK':            100,
+    'IMAGE_SYM_CLASS_FUNCTION':         101,
+    'IMAGE_SYM_CLASS_END_OF_STRUCT':    102,
+    'IMAGE_SYM_CLASS_FILE':             103,
+    'IMAGE_SYM_CLASS_SECTION':          104,
+    'IMAGE_SYM_CLASS_WEAK_EXTERNAL':    105,
+    'IMAGE_SYM_CLASS_CLR_TOKEN':        107
   }
 
 RelocationTypes = {
-    'IMAGE_REL_I386_DIR32': 6
+    'IMAGE_REL_I386_ABSOLUTE':  0x0000,
+    'IMAGE_REL_I386_DIR16':     0x0001,
+    'IMAGE_REL_I386_REL16':     0x0002,
+    'IMAGE_REL_I386_DIR32':     0x0006,
+    'IMAGE_REL_I386_DIR32NB':   0x0007,
+    'IMAGE_REL_I386_SEG12':     0x0009,
+    'IMAGE_REL_I386_SECTION':   0x000A,
+    'IMAGE_REL_I386_SECREL':    0x000B,
+    'IMAGE_REL_I386_TOKEN':     0x000C,
+    'IMAGE_REL_I386_SECREL7':   0x000D,
+    'IMAGE_REL_I386_REL32':     0x0014,
+
+    'IMAGE_REL_AMD64_ABSOLUTE': 0x0000,
+    'IMAGE_REL_AMD64_ADDR64':   0x0001,
+    'IMAGE_REL_AMD64_ADDR32':   0x0002,
+    'IMAGE_REL_AMD64_ADDR32NB': 0x0003,
+    'IMAGE_REL_AMD64_REL32':    0x0004,
+    'IMAGE_REL_AMD64_REL32_1':  0x0005,
+    'IMAGE_REL_AMD64_REL32_2':  0x0006,
+    'IMAGE_REL_AMD64_REL32_3':  0x0007,
+    'IMAGE_REL_AMD64_REL32_4':  0x0008,
+    'IMAGE_REL_AMD64_REL32_5':  0x0009,
+    'IMAGE_REL_AMD64_SECTION':  0x000A,
+    'IMAGE_REL_AMD64_SECREL':   0x000B,
+    'IMAGE_REL_AMD64_SECREL7':  0x000C,
+    'IMAGE_REL_AMD64_TOKEN':    0x000D,
+    'IMAGE_REL_AMD64_SREL32':   0x000E,
+    'IMAGE_REL_AMD64_PAIR':     0x000F,
+    'IMAGE_REL_AMD64_SSPAN32':  0x0010
+
+    'IMAGE_REL_ARM_ABSOLUTE':   0x0000,
+    'IMAGE_REL_ARM_ADDR32':     0x0001,
+    'IMAGE_REL_ARM_ADDR32NB':   0x0002,
+    'IMAGE_REL_ARM_BRANCH24':   0x0003,
+    'IMAGE_REL_ARM_BRANCH11':   0x0004,
+    'IMAGE_REL_ARM_TOKEN':      0x0005,
+    'IMAGE_REL_ARM_BLX24':      0x0008,
+    'IMAGE_REL_ARM_BLX11':      0x0009,
+    'IMAGE_REL_ARM_SECTION':    0x000E,
+    'IMAGE_REL_ARM_SECREL':     0x000F,
+    'IMAGE_REL_ARM_MOV32A':     0x0010,
+    'IMAGE_REL_ARM_MOV32T':     0x0011,
+    'IMAGE_REL_ARM_BRANCH20T':  0x0012,
+    'IMAGE_REL_ARM_BRANCH24T':  0x0014,
+    'IMAGE_REL_ARM_BLX23T':     0x0015
   }
 
 class COFFHeader(yaml.YAMLObject):
@@ -864,10 +1016,12 @@ class COFF:
     curoffset = SIZEOF_HEADER + self.header.NumberOfSections * SIZEOF_SECTION
     for i, s in enumerate(self.sections):
       s.layout(self, i)
-      s.PointerToRawData = curoffset
-      curoffset += s.SizeOfRawData
-      s.PointerToRelocations = curoffset
-      curoffset += s.NumberOfRelocations * SIZEOF_RELOCATION
+      if s.PointerToRawData != 0:
+        s.PointerToRawData = curoffset
+        curoffset += s.SizeOfRawData
+      if s.PointerToRelocations != 0:
+        s.PointerToRelocations = curoffset
+        curoffset += s.NumberOfRelocations * SIZEOF_RELOCATION
     self.header.PointerToSymbolTable = curoffset
 
     index = 0
