862e652fb2b0b1d878ba695c658b3319af37c437
Michael J. Spencer <bigcheesegs@gmail.com>
2012-03-04 19:19:44 -0800

    SDaganodes fixblah.

--- lib/CodeGen/SelectionDAG/ScheduleDAGFast.cpp
+++ lib/CodeGen/SelectionDAG/ScheduleDAGFast.cpp
@@ -12,8 +12,8 @@
 //===----------------------------------------------------------------------===//
 
 #define DEBUG_TYPE "pre-RA-sched"
-#include "ScheduleDAGSDNodes.h"
 #include "llvm/InlineAsm.h"
+#include "llvm/CodeGen/ScheduleDAGSDNodes.h"
 #include "llvm/CodeGen/SchedulerRegistry.h"
 #include "llvm/CodeGen/SelectionDAGISel.h"
 #include "llvm/Target/TargetRegisterInfo.h"
