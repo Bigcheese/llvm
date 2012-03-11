fde078c8908ee69c91a7ab630467db68d5b7b8c3
Michael J. Spencer <bigcheesegs@gmail.com>
2012-03-10 18:45:29 -0800

    Cleanup MFI.

--- lib/Target/AIObj/AIObjMachineFunctionInfo.h
+++ lib/Target/AIObj/AIObjMachineFunctionInfo.h
@@ -19,29 +19,10 @@ namespace llvm {
 
   class AIObjMachineFunctionInfo : public MachineFunctionInfo {
     virtual void anchor();
-  private:
-    unsigned GlobalBaseReg;
 
-    /// VarArgsFrameOffset - Frame offset to start of varargs area.
-    int VarArgsFrameOffset;
-
-    /// SRetReturnReg - Holds the virtual register into which the sret
-    /// argument is passed.
-    unsigned SRetReturnReg;
   public:
-    AIObjMachineFunctionInfo()
-      : GlobalBaseReg(0), VarArgsFrameOffset(0), SRetReturnReg(0) {}
-    explicit AIObjMachineFunctionInfo(MachineFunction &MF)
-      : GlobalBaseReg(0), VarArgsFrameOffset(0), SRetReturnReg(0) {}
-
-    unsigned getGlobalBaseReg() const { return GlobalBaseReg; }
-    void setGlobalBaseReg(unsigned Reg) { GlobalBaseReg = Reg; }
-
-    int getVarArgsFrameOffset() const { return VarArgsFrameOffset; }
-    void setVarArgsFrameOffset(int Offset) { VarArgsFrameOffset = Offset; }
-
-    unsigned getSRetReturnReg() const { return SRetReturnReg; }
-    void setSRetReturnReg(unsigned Reg) { SRetReturnReg = Reg; }
+    AIObjMachineFunctionInfo() {}
+    explicit AIObjMachineFunctionInfo(MachineFunction &MF) {}
   };
 }
 
